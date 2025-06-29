839cb4560d89d4de220693c969be865320b2d458
sayandas-sd <sayandasmac@gmail.com>
2025-06-30 01:04:04 +0530

    feat(api): serve index.html on GET / with proper error handling

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,19 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn get_data() -> impl Responder {
-    HttpResponse::Ok().body("hey, welcome to our page")
+
+    let serverdir = std::fs::read_to_string("index.html");
+
+    match serverdir {
+        Ok(html) => HttpResponse::Ok()
+            .content_type("text/html; charset=utf-8")
+            .body(html),
+        Err(e) => {
+            eprintln!("erroor :{}", e);
+            HttpResponse::InternalServerError().body("something is wrong")
+        }
+
+    }
 }
 
 
@@ -20,13 +32,17 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+
+    let port = "0.0.0.0:8080";
+    println!("Server is Running on http://{}", port);
+
     HttpServer::new(|| {
         App::new()
             .service(get_data)
             .service(post_data)
             .route("/health", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(port)?
     .run()
     .await
 }
